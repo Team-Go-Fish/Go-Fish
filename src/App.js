7c9c96d6dae9625e97782ea12a66ade0b0dd30ec
spacerumsfeld-code <nickfin2014@gmail.com>
2021-04-03 11:12:11 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,25 +4,29 @@ import './App.css';
 const App = () = {
   return (
     <div className="App">
+     
       <div className="login-container">
       // do login / signup shit
       </div>
     
-    <div className="button-panel">
-     // notification
-     //. gofish
-     // friends
+      <div className="button-panel">
+       // notification
+       //. gofish
+       // friends
+      </div>
+
+      <div className="search-bar">
+      </div>
+
+     <div className="lists">
+        <div className="movie-list">
+        </div>
+
+        <div className="watch-list">
+     </div>
+     
     </div>
-    
-    <div className="search-bar">
-    
-    </div>
-    
-    <div className="movie-list">
-    </div>
-    
-    <div className="watch-list">
-    </div>
+  </div>
   );
 }
 
