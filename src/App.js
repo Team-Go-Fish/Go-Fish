10955ea89fa7a4a288725d74b2027e5e04896e4d
Nick Papadakis <nickfin2014@gmail.com>
2021-04-05 14:39:34 -0700

    added parent component for lists and basic glider functionality for lists

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import './App.css';
+import './App.scss';
 import ButtonPanel from './Components/Button-Panel/ButtonPanel.js'
 
 import ListsAndSearch from './Components/ListsAndSearch/ListsAndSearch.js';
--- src/Components/ListsAndSearch/MovieList.js
+++ src/Components/ListsAndSearch/MovieList.js
@@ -1,12 +1,29 @@
 import { useState, useEffect } from 'react';
+import Glide from "@glidejs/glide";
+
+const sliderConfiguration = {
+  gap: 10,
+  perView: 4,
+  startAt: 0,
+  type: "slider"
+};
 
 const MovieList = ({ movies }) => {
+  const slider = new Glide('.glide', sliderConfiguration);
+
+  useEffect(() => {
+    return () => slider.mount();
+  }, [slider])
+
   return (
-    <div className="movie-list">
-        <ul className="holla">
+    <>
+      {" "}
+      <div className='glide'>
+        <div className='glide__track' data-glide-el='track'>
+          <ul className='glide__slides'>
         {
          movies.map((movie) => {
-           return <div className="movie">
+           return <div className="glide__slide slider">
              <p>{movie.title}</p>
              <p>{movie.vote_average}</p>
              <img src={movie.poster_path} alt="" />
@@ -14,8 +31,39 @@ const MovieList = ({ movies }) => {
          })
         }
         </ul>
-    </div>
+      </div>
+        <div className='glide__arrows' data-glide-el='controls'>
+          <button
+            className='glide__arrow glide__arrow--prev'
+            data-glide-dir='<'
+          >
+            prev
+          </button>
+          <button
+            className='glide__arrow glide__arrow--next'
+            data-glide-dir='>'
+          >
+            next
+          </button>
+        </div>
+      </div>
+    </>
   );
+  // return (
+  //   <div className="movie-list">
+  //       <ul className="holla">
+  //       {
+  //        movies.map((movie) => {
+  //          return <div className="movie">
+  //            <p>{movie.title}</p>
+  //            <p>{movie.vote_average}</p>
+  //            <img src={movie.poster_path} alt="" />
+  //          </div>
+  //        })
+  //       }
+  //       </ul>
+  //   </div>
+  // );
 };
 
 export default MovieList;
