65850c853bb1d41685a7f0eba02d1d40eb310273
Mason Hoffman <mhoffman3939@gmail.com>
2021-04-05 16:50:24 -0700

    Got "checked" to update.

--- src/Components/Button-Panel/FriendEntry.js
+++ src/Components/Button-Panel/FriendEntry.js
@@ -1,16 +1,39 @@
 import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
 import axios from 'axios';
 
-const FriendEntry = ({ user }) => {
-  // state
+class FriendEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: {}
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
   // methods
+  handleChange(key, event) {
+    if (this.state.checked === true) {
+      this.setState({checked: false});
+    } else {
+      this.setState({checked: true})
+    }
+  }
 
-  return (
-    <div>
-      <span>{user.firstName} {user.lastName}</span>
-    </div>
-  )
+  render() {
+    return (
+      <Form>
+        <div key={'default-checkbox'} className="mb-3">
+          <Form.Check
+            type='checkbox'
+            key={this.props.user.firstName}
+            onChange = {this.handleChange.bind(this)}
+            label={<span>{this.props.user.firstName} {this.props.user.lastName}</span>}
+          />
+        </div>
+      </Form>
+    )
+  }
 };
 
 export default FriendEntry;
--- src/Components/Button-Panel/FriendList.js
+++ src/Components/Button-Panel/FriendList.js
@@ -16,3 +16,5 @@ const FriendList = ({ users }) => {
 };
 
 export default FriendList;
+
+
