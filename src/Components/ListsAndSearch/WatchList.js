d0f3533060a279e9e4066df45e272bf1ae7517f0
Nick Papadakis <nickfin2014@gmail.com>
2021-04-06 15:28:33 -0700

    added conditional rendering of Watchlist if user is logged in

--- src/Components/ListsAndSearch/WatchList.js
+++ src/Components/ListsAndSearch/WatchList.js
@@ -1,59 +1,63 @@
-// import { useState, useEffect } from 'react';
-// //import Glide from "@glidejs/glide";
-// import { Card, Button, Container, Row, Col } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+//import Glide from "@glidejs/glide";
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 
-// import Slider from 'react-slick';
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-// const WatchList = ({ myMovies }) => {
-//     // const slider = new Glide('.glide', sliderConfiguration);
+const WatchList = ({ myMovies, user }) => {
+    // const slider = new Glide('.glide', sliderConfiguration);
 
-//     // useEffect(() => {
-//     //   return () => slider.mount();
-//     // }, [slider])
-//     const settings = {
-//       dots: false,
-//       infinite: true,
-//       speed: 500,
-//       slidesToShow: 4,
-//       slidesToScroll: 2,
-//       spacing: 2
-//     };
+    // useEffect(() => {
+    //   return () => slider.mount();
+    // }, [slider])
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 2,
+      spacing: 2
+    };
 
-//     return (
-//       <>
-//       <Container>
-//         <Row>
-//           <Col>
+    return (
+      <>
+      {(user) && (
+        <>
+        <Container>
+          <Row>
+            <Col>
 
-//         <Slider {...settings}>
-//           {
-//             movies.map((movie) => {
-//               return (
-//                 <Card style={{ width: '18rem' }}>
-//                   <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-//                   <Card.Body>
-//                     <Card.Header>{movie.title}</Card.Header>
-//                     <Card.Text>
-//                       {movie.vote_average}
-//                     </Card.Text>
-//                     <Card.Text>
-//                       {movie.overview}
-//                     </Card.Text>
-//                     <Button variant="primary">Add to my list</Button>
-//                   </Card.Body>
-//                 </Card>
-//               )
-//             })
-//           }
+          <Slider {...settings}>
+            {
+              myMovies.map((movie) => {
+                return (
+                  <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                    <Card.Body>
+                      <Card.Header>{movie.title}</Card.Header>
+                      <Card.Text>
+                        {movie.vote_average}
+                      </Card.Text>
+                      <Card.Text>
+                        {movie.overview}
+                      </Card.Text>
+                      <Button variant="primary">Add to my list</Button>
+                    </Card.Body>
+                  </Card>
+                )
+              })
+            }
 
-//         </Slider>
-//           </Col>
-//         </Row>
-//       </Container>
-//       </>
-//     );
-// };
+          </Slider>
+            </Col>
+          </Row>
+        </Container>
+        </>
+      )}
+      </>
+    );
+};
 
-// export default WatchList;
+export default WatchList;
