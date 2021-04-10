5086f035670b26518c5252aa1227c581dc071afd
Mason Hoffman <mhoffman3939@gmail.com>
2021-04-10 09:48:07 -0700

    Added comments and code cleanup.

--- src/Components/Button-Panel/FriendList.js
+++ src/Components/Button-Panel/FriendList.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import FriendEntry from './FriendEntry.js';
 
+/**if a user has friends, this components maps over the input array and passes
+ * each individual friend to FriendEntry. If the user has no friends, this component
+ * just passes props to FriendEntry */
 const FriendList = ({ userID, friends, setSelected }) => {
   let hasFriends;
   if (friends) {
